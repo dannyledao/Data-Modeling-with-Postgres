909a985cbb23203c0ab8328580858715bb71cf18
dannyledao <43320386+dannyledao@users.noreply.github.com>
2022-01-21 04:36:41 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -6,6 +6,13 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    
+    """
+    Inserting artist and song record into song and artist tables
+    cur (psycopg2.cursor): PostgresSQL database cursor
+    filepath (str): A filepath to a song file
+    """
+    
     # open song file
     df = pd.read_json(filepath, lines =True)
 
@@ -28,6 +35,16 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """Processes a log file.
+    
+   
+    Inserting time and user records into time and user tables
+    cur (psycopg2.cursor): PostgresSQL database cursor
+    filepath (str): A filepath to a song file
+    """
+    
+    
+    
     # open log file
     df = pd.read_json(filepath,lines=True)
 
@@ -76,6 +93,15 @@ def process_log_file(cur, filepath):
      
 
 def process_data(cur, conn, filepath, func):
+   """ Process all JSON files in data directory path in both song and log database.
+        cur (psycopg2.cursor): PostgreSQL database cursor
+        conn (psycopg2.connection): Establishing connection with sparkify database
+        filepath (str): directory
+        func (function): processing function
+    """
+
+
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -95,6 +121,10 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """This class/method created to establish connection with database and process song and log data using methods above.
+    The class will close the curse and connection with database once finish looping/processing through all JSON files.
+ 
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
